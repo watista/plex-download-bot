86f34a35318d66a577945fdb814d23255edf378e
Wouter <prive@wouterpaas.nl>
2025-03-03 19:41:46 +0100

    add try except with recommended wait time

--- src/functions.py
+++ src/functions.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 import re
-
+import asyncio
+from telegram.error import RetryAfter
 
 class Functions:
 
@@ -16,14 +17,23 @@ class Functions:
 
         # Check if the user_id is regular or special
         chat_id = update.effective_user.id if regular else update
-
-        await context.bot.send_message(
-            chat_id=chat_id,
-            text=self.escape_markdown(text, parse_mode),
-            parse_mode=parse_mode,
-            reply_markup=reply_markup,
-            disable_web_page_preview=True
-        )
+        try:
+            await context.bot.send_message(
+                chat_id=chat_id,
+                text=self.escape_markdown(text, parse_mode)[:1024],
+                parse_mode=parse_mode,
+                reply_markup=reply_markup,
+                disable_web_page_preview=True
+            )
+        except RetryAfter as e:
+            await asyncio.sleep(e.retry_after)
+            await context.bot.send_message(
+                chat_id=chat_id,
+                text=self.escape_markdown(text, parse_mode)[:1024],
+                parse_mode=parse_mode,
+                reply_markup=reply_markup,
+                disable_web_page_preview=True
+            )
 
         # Debug log
         await self.log.logger(text, False, "debug", False)
@@ -31,13 +41,23 @@ class Functions:
 
     # Send message with GIF
     async def send_gif(self, caption: str, animation, update, context, reply_markup=None, parse_mode='MarkdownV2') -> None:
-        await context.bot.send_animation(
-            chat_id=update.effective_user.id,
-            caption=self.escape_markdown(caption, parse_mode),
-            animation=animation,
-            parse_mode=parse_mode,
-            reply_markup=reply_markup
-        )
+        try:
+            await context.bot.send_animation(
+                chat_id=update.effective_user.id,
+                caption=self.escape_markdown(caption, parse_mode)[:1024],
+                animation=animation,
+                parse_mode=parse_mode,
+                reply_markup=reply_markup
+            )
+        except RetryAfter as e:
+            await asyncio.sleep(e.retry_after)
+            await context.bot.send_animation(
+                chat_id=update.effective_user.id,
+                caption=self.escape_markdown(caption, parse_mode)[:1024],
+                animation=animation,
+                parse_mode=parse_mode,
+                reply_markup=reply_markup
+            )
 
         # Debug log
         await self.log.logger(caption, False, "debug", False)
@@ -45,13 +65,23 @@ class Functions:
 
     # Send message with image
     async def send_image(self, caption: str, photo, update, context, reply_markup=None, parse_mode='MarkdownV2') -> None:
-        await context.bot.send_photo(
-            chat_id=update.effective_user.id,
-            caption=self.escape_markdown(caption, parse_mode)[:1024],
-            photo=photo,
-            parse_mode=parse_mode,
-            reply_markup=reply_markup
-        )
+        try:
+            await context.bot.send_photo(
+                chat_id=update.effective_user.id,
+                caption=self.escape_markdown(caption, parse_mode)[:1024],
+                photo=photo,
+                parse_mode=parse_mode,
+                reply_markup=reply_markup
+            )
+        except RetryAfter as e:
+            await asyncio.sleep(e.retry_after)
+            await context.bot.send_photo(
+                chat_id=update.effective_user.id,
+                caption=self.escape_markdown(caption, parse_mode)[:1024],
+                photo=photo,
+                parse_mode=parse_mode,
+                reply_markup=reply_markup
+            )
 
         # Debug log
         await self.log.logger(caption, False, "debug", False)
